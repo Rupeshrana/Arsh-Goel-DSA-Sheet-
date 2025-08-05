718863d5993a187c53149b2f4f9a314afeee33c1
Rupeshrana <130509170+Rupeshrana@users.noreply.github.com>
2025-08-05 16:00:14 +0530

    Time: 3 ms (29.02%), Space: 17.3 MB (19.48%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<pair<int,int>,int>> rotten;
+       vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        int cntfresh=0;
+        for( int i=0;i<n;i++){
+            for( int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    rotten.push({{i,j},0});
+                    vis[i][j]=2;
+
+                }
+                else{
+                    vis[i][j]=0;
+
+                }
+                if(grid[i][j]==1){
+                    cntfresh++;
+                }
+            }
+        }
+        int tm=0;
+        int cnt=0;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        while(!rotten.empty()){
+            int r=rotten.front().first.first;
+            int c=rotten.front().first.second;
+            int t=rotten.front().second;
+            tm=max(tm,t);
+            rotten.pop();
+            for( int i=0;i<4;i++){
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    rotten.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=2;
+                    cnt++;
+                }
+            }      
+      }
+      if( cnt!=cntfresh) return -1;
+
+        return tm;
+    }
+};
